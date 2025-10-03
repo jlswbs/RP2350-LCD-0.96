567b11c8249c6aff9e2e4ba95040edf546e4e1b7
jlswbs <jlswbs@users.noreply.github.com>
2025-10-03 13:08:48 +0200

    Add files via upload

---
+++ Pattern_Chaotic/Pattern_Chaotic.ino
@@ -0,0 +1,87 @@
+// Patterns from simplest chaotic map //
+
+#include "hardware/structs/rosc.h"
+#include "ST7735_TFT.hpp"
+
+#define WIDTH   80
+#define HEIGHT  160
+#define SCR     WIDTH*HEIGHT
+
+#define PIN_SCK    10
+#define PIN_MOSI   11
+#define PIN_RST    12
+#define PIN_CS     9
+#define PIN_DC     8
+
+  ST7735_TFT tft;
+
+#define CNUM    32
+
+  float x = 0.1f;
+  float a = 2.0f;
+
+  uint16_t color[CNUM];
+
+float randomf(float minf, float maxf) {return minf + (rand()%(1UL << 31))*(maxf - minf) / (1UL << 31);}
+
+static inline void seed_random_from_rosc(){
+  
+  uint32_t random = 0;
+  uint32_t random_bit;
+  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+
+  for (int k = 0; k < 32; k++) {
+    while (1) {
+      random_bit = (*rnd_reg) & 1;
+      if (random_bit != ((*rnd_reg) & 1)) break;
+    }
+
+    random = (random << 1) | random_bit;
+  }
+
+  srand(random);
+
+}
+
+void rndseed(){
+
+  for (int i = 0; i < CNUM; i++) color[i] = rand();
+
+  a = randomf(1.499f, 2.0f);
+
+}
+
+void setup(){
+
+  seed_random_from_rosc();
+
+  tft.TFTInitSPIType(62500, spi1);
+	tft.TFTSetupGPIO(PIN_RST, PIN_DC, PIN_CS, PIN_SCK, PIN_MOSI);
+  tft.TFTInitScreenSize(26, 1, WIDTH, HEIGHT);
+	tft.TFTInitPCBType(TFT_PCBtype_e::TFT_ST7735S_Black);
+  tft.TFTchangeInvertMode(true);
+	tft.TFTfillScreen(ST7735_BLACK);
+  
+  rndseed();
+  
+}
+
+void loop(){
+  
+  for (int j = 0; j < HEIGHT; j++) {
+
+    for (int i = 0; i < WIDTH; i++) {
+
+      float nx = x;
+
+      x = a - powf(nx, 2.0f);
+
+      uint8_t coll = (CNUM/2) + ((CNUM/2) * x);
+
+      tft.pushColor(color[coll%CNUM]);
+
+    }
+
+  }
+
+}
