84d9c5eda78800013e7d6a56320976b72c8dae29
jlswbs <jlswbs@users.noreply.github.com>
2025-10-05 22:49:58 +0200

    Add files via upload

---
+++ 2D_Mandala/2D_Mandala.ino
@@ -0,0 +1,110 @@
+// 2D Mandala patterns //
+
+#include "hardware/structs/rosc.h"
+#include "ST7735_TFT.hpp"
+
+#define WIDTH   80
+#define HEIGHT  160
+#define SCR     WIDTH*HEIGHT
+
+#define PIN_SCK    10
+#define PIN_MOSI   11
+#define PIN_RST    12
+#define PIN_CS     9
+#define PIN_DC     8
+
+  ST7735_TFT tft;
+
+#define CNUM    32
+
+  float a = 0.25f;
+  float p[WIDTH][HEIGHT];
+  float v[WIDTH][HEIGHT];
+  uint16_t color[CNUM];
+
+float randomf(float minf, float maxf) {return minf + (rand()%(1UL << 31))*(maxf - minf) / (1UL << 31);}
+
+static inline void seed_random_from_rosc(){
+  
+  uint32_t random = 0;
+  uint32_t random_bit;
+  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+
+  for (int k = 0; k < 32; k++) {
+    while (1) {
+      random_bit = (*rnd_reg) & 1;
+      if (random_bit != ((*rnd_reg) & 1)) break;
+    }
+
+    random = (random << 1) | random_bit;
+  }
+
+  srand(random);
+
+}
+
+void rndseed(){
+
+  for (int y = 0; y < HEIGHT; y++) {
+
+    for (int x = 0; x < WIDTH; x++) {
+
+      p[x][y] = 0;
+      v[x][y] = 0;
+    
+    }
+  
+  }
+
+  a = randomf(-0.25f, 0.25f);
+  float seed = randomf(-TWO_PI, TWO_PI);
+
+  v[WIDTH/2][HEIGHT/2] = seed;
+  v[WIDTH/2][(HEIGHT/2)-1] = seed;
+  v[(WIDTH/2)-1][(HEIGHT/2)-1] = seed;
+  v[(WIDTH/2)-1][HEIGHT/2] = seed;
+
+  for (int i = 0; i < CNUM; i++) color[i] = rand();
+
+}
+
+void setup(){
+
+  seed_random_from_rosc();
+
+  tft.TFTInitSPIType(62500, spi1);
+	tft.TFTSetupGPIO(PIN_RST, PIN_DC, PIN_CS, PIN_SCK, PIN_MOSI);
+  tft.TFTInitScreenSize(26, 1, WIDTH, HEIGHT);
+	tft.TFTInitPCBType(TFT_PCBtype_e::TFT_ST7735S_Black);
+  tft.TFTchangeInvertMode(true);
+	tft.TFTfillScreen(ST7735_BLACK);
+  
+  rndseed();
+  
+}
+
+void loop(){
+  
+  for (int y = 1; y < HEIGHT-1; y++) {
+
+    for (int x = 1; x < WIDTH-1; x++) {
+
+      v[x][y] += sinf(p[x+1][y+1] + p[x-1][y-1] + p[x+1][y-1] + p[x-1][y+1]) * a - (p[x][y]);
+    
+    }
+  
+  }
+
+  for (int y = 0; y < HEIGHT; y++) {
+
+    for (int x = 0; x < WIDTH; x++) {
+
+      p[x][y] += v[x][y];
+      uint8_t coll = CNUM - (CNUM * p[x][y]);
+      tft.pushColor(color[coll%CNUM]);
+
+    }
+  
+  }
+
+}
