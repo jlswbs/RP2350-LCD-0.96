c7b7621163969811b0c7a559e5f6a82b25937b4a
jlswbs <jlswbs@users.noreply.github.com>
2025-10-07 15:06:33 +0200

    Add files via upload

---
+++ 2D_Chladni/2D_Chladni.ino
@@ -0,0 +1,99 @@
+// 2D Chladni patterns //
+
+#include "hardware/structs/rosc.h"
+#include "ST7735_TFT.hpp"
+
+#define WIDTH   80
+#define HEIGHT  160
+#define SCR     WIDTH*HEIGHT
+
+#define PIN_SCK    10
+#define PIN_MOSI   11
+#define PIN_RST    12
+#define PIN_CS     9
+#define PIN_DC     8
+
+  ST7735_TFT tft;
+
+#define NUMS    16
+
+  float p[WIDTH][HEIGHT];
+  float v[WIDTH][HEIGHT];
+  uint16_t color[NUMS];
+  float a, b, c;
+
+float randomf(float minf, float maxf) {return minf + (rand()%(1UL << 31))*(maxf - minf) / (1UL << 31);} 
+
+static inline void seed_random_from_rosc(){
+  
+  uint32_t random = 0;
+  uint32_t random_bit;
+  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+
+  for (int k = 0; k < 32; k++) {
+    while (1) {
+      random_bit = (*rnd_reg) & 1;
+      if (random_bit != ((*rnd_reg) & 1)) break;
+    }
+
+    random = (random << 1) | random_bit;
+  }
+
+  srand(random);
+}
+
+void rndseed(){
+
+  a = randomf(0.005f, 0.099f);
+  b = randomf(0.049f, 0.199f);
+  c = randomf(1.0f, 12.0f);
+
+  for (int i = 0; i < NUMS; i++) color[i] = rand();
+  
+}
+
+void setup(){
+
+  seed_random_from_rosc();
+
+  tft.TFTInitSPIType(62500, spi1);
+	tft.TFTSetupGPIO(PIN_RST, PIN_DC, PIN_CS, PIN_SCK, PIN_MOSI);
+  tft.TFTInitScreenSize(26, 1, WIDTH, HEIGHT);
+	tft.TFTInitPCBType(TFT_PCBtype_e::TFT_ST7735S_Black);
+  tft.TFTchangeInvertMode(true);
+	tft.TFTfillScreen(ST7735_BLACK);
+  
+  rndseed();
+  
+}
+
+void loop(){
+
+  int frame = millis() / 32;
+
+  v[WIDTH/2][HEIGHT/2] = 0;
+  p[WIDTH/2][HEIGHT/2] = (sinf(frame * a) + sinf(frame * b)) * c;
+  
+  for (int y = 1; y < HEIGHT-1; y++) {
+
+    for (int x = 1; x < WIDTH-1; x++) {
+
+      v[x][y] += (p[x-1][y] + p[x+1][y] + p[x][y-1] + p[x][y+1]) * 0.25f - p[x][y];
+
+    }
+  
+  }
+  
+  for (int y = 0; y < HEIGHT; y++){
+
+    for (int x = 0; x < WIDTH; x++){
+
+      p[x][y] += v[x][y];
+      uint8_t coll = NUMS * fabs(constrain(p[x][y], -1.0f, 1.0f));
+      tft.pushColor(color[coll % NUMS]);
+    
+    }
+  
+  }
+
+}
