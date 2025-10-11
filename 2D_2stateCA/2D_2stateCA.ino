a8d5173de46fc02b1bfc12d374bdb291ff8c32b3
jlswbs <jlswbs@users.noreply.github.com>
2025-10-11 14:02:59 +0200

    Add files via upload

--- 2D_2stateCA/2D_2stateCA.ino
+++ 2D_2stateCA/2D_2stateCA.ino
@@ -1,4 +1,4 @@
-// 2D Cellular automata 3 states //
+// 2D Cellular automata 2 states //
 
 #include "hardware/structs/rosc.h"
 #include "ST7735_TFT.hpp"
@@ -42,7 +42,7 @@ void rndseed(){
 
   for (int y = 0; y < HEIGHT; y++) {
 
-    for (int x = 0; x < WIDTH; x++) grid[x][y] = random(3);
+    for (int x = 0; x < WIDTH; x++) grid[x][y] = random(2);
 
   }
 
@@ -51,18 +51,13 @@ void rndseed(){
 uint8_t count_neighbors(uint8_t x, uint8_t y, uint8_t state) {
 
   uint8_t count = 0;
-
   for (int8_t i = -1; i <= 1; i++) {
-
     for (int8_t j = -1; j <= 1; j++) {
-
       if (i == 0 && j == 0) continue;
       uint8_t nx = (x + i + WIDTH) % WIDTH;
       uint8_t ny = (y + j + HEIGHT) % HEIGHT;
       if (grid[nx][ny] == state) count++;
-
     }
-
   }
 
   return count;
@@ -76,19 +71,12 @@ void update_grid() {
     for (uint8_t x = 0; x < WIDTH; x++) {
 
       uint8_t current_state = grid[x][y];
-      uint8_t neighbors[3];
-      for (uint8_t s = 0; s < 3; s++) {
-        neighbors[s] = count_neighbors(x, y, s);
-      }
+      uint8_t neighbors = count_neighbors(x, y, 1);
       new_grid[x][y] = current_state;
-      if (current_state == 0) {
-        if (neighbors[1] >= 3) new_grid[x][y] = 1;
-      } else if (current_state == 1) {
-        if (neighbors[2] >= 3) new_grid[x][y] = 2;
-        else if (neighbors[1] < 2) new_grid[x][y] = 0;
-      } else if (current_state == 2) {
-        if (neighbors[0] >= 3) new_grid[x][y] = 0;
-        else if (neighbors[2] < 2) new_grid[x][y] = 1;
+      if (current_state == 1) {
+        if (neighbors < 2 || neighbors > 3) new_grid[x][y] = 0;
+      } else {
+        if (neighbors == 3) new_grid[x][y] = 1;
       }
 
     }
@@ -126,9 +114,8 @@ void loop(){
 
     for (int x = 0; x < WIDTH; x++) {
 
-      if (grid[x][y] == 0) color = ST7735_BLUE;
-      if (grid[x][y] == 1) color = ST7735_RED;
-      if (grid[x][y] == 2) color = ST7735_WHITE;
+      if (grid[x][y] == 0) color = ST7735_BLACK;
+      if (grid[x][y] == 1) color = ST7735_WHITE;
       tft.pushColor(color);
 
     }
